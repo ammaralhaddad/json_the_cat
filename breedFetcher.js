be6e68f5afcf28a3fa1542fa5969eb4251bc2dce
AMMAR AL HADDAD <91353209+ammaralhaddad@users.noreply.github.com>
2021-11-04 15:02:24 -0400

    Update breedFetcher.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,17 @@
 const request = require('request');
-const input =  process.argv[2]
 
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${input}`, (error, response, body) => {
+
+const fetchBreedDescription = function(breedName, callback) {
+
+
+
+request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
   
-  // console.log(" this this type of body",typeof body)
+ 
 
   const data = JSON.parse(body);
-  // console.log("data",data);
-  // console.log("typeof data",typeof data);
-
-//  let descriptionOfCat = data[0].description
-
-//  console.log("description",descriptionOfCat)
+  
  if(data[0]){
 console.log("description",data[0].description)
 
@@ -23,5 +22,6 @@ console.log("description",data[0].description)
 
 });
 
+};
 
-
+module.exports = { fetchBreedDescription };
